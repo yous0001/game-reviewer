c1d97dfb4ad581ff317cbdef9c6e4162973ec061
Yousef emad mikhail <mad33833@gmail.com>
2024-08-03 19:11:41 +0300

    add games section

--- js/ui.js
+++ js/ui.js
@@ -1,2 +1,32 @@
 //display data
 //display details
+
+async function getGames(category){
+    const url = `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'c67815b3d8mshed2de66a8928b04p19cbb4jsn83c3518bdaaf',
+            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+    let response = await fetch(url, options);
+    response=response.json()
+    console.log(response);
+    
+}
+
+async function getGameDetails(id){
+    const url = `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'c67815b3d8mshed2de66a8928b04p19cbb4jsn83c3518bdaaf',
+            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+    let response = await fetch(url, options);
+    response=response.json()
+    console.log(response);
+    
+}
